6a914b969572f54273a7b85cff806ea0112ae477
Rémi Tétreault <tetreault.remi@gmail.com>
2016-09-14 03:23:58 -0400

    Add new features to the applet
    First of all, this commit adds two new spawn functions. Theses functions
    replace the one from the Cinnamon 'misc.util' library. The biggest change that
    these functions bring is that they pass the exit code of the program that they
    executed to the callback. This allows to detect if an error occurred which was
    not possible before.
    
    A new feature is now available on this applet. When the user click on the
    applet, the Derytelecom Extranet login page opens in the default web browser.
    If the new 'Auto-Login' checkbox in this applet configuration is checked, this
    applet will try to fill the login form using the provided username and
    password. This only works if the PyUserInput package is installed.
    
    Another new feature is that the color of the text displayed by this applet
    change depending on whether the last transaction with the Derytelecom Extranet
    was successful or not. By default, if it was successful, the text is green else
    it is red. These colors can be changed in the configuration of this applet.

--- applet.js
+++ applet.js
@@ -2,6 +2,7 @@
 // Imports
 //----------------------------------------------------------------------
 const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
 const Mainloop = imports.mainloop;
 const Lang = imports.lang;
 
@@ -14,22 +15,136 @@ const Util = imports.misc.util;
 //----------------------------------------------------------------------
 // Constants
 //----------------------------------------------------------------------
-const UUID = "derytelecomavailabledata@remi.tetreault"
-const PYTHON_SCRIPT_NAME = "/derytelecomextranetquery";
+const PYTHON_SCRIPT_NAME = "derytelecomextranetquery";
 
+const G_SPAWN_EXIT_ERROR =  GLib.spawn_exit_error_quark ();
+const G_SPAWN_ERROR = GLib.spawn_error_quark ();
+
+
+
+
+//----------------------------------------------------------------------
+// Custom Spawn
+//----------------------------------------------------------------------
+// Spawn the program in argv and call the callback when the program
+// return an exit code. The exit code is passed to the callback.
+function spawn_async(argv, callback) {
+    try {
+        var [success, pid] =
+            GLib.spawn_async(null, argv, null,
+                             GLib.SpawnFlags.SEARCH_PATH
+                             | GLib.SpawnFlags.DO_NOT_REAP_CHILD
+                             | GLib.SpawnFlags.STDOUT_TO_DEV_NULL
+                             | GLib.SpawnFlags.STDERR_TO_DEV_NULL,
+                             null);
+    } catch (err) {
+        Util._handleSpawnError(argv[0], err);
+    }
+
+
+    if (success) {
+        GLib.child_watch_add(GLib.PRIORITY_DEFAULT_IDLE, pid,
+            function (pid, status) {
+                var is_fatal_error = false;
+                var exitcode = 0;
+
+                try {
+                    GLib.spawn_check_exit_status(status);
+                } catch(err) {
+                    if (err instanceof GLib.Error &&
+                            err.domain === G_SPAWN_EXIT_ERROR) {
+                        exitcode = err.code;
+                    } else {
+                        Util._handleSpawnError(argv[0], err);
+                        is_fatal_error = true;
+                    }
+                }
+
+                if (!is_fatal_error) {
+                    callback(exitcode);
+                }
+
+                GLib.spawn_close_pid(pid);
+            }
+        );
+    }
+}
+
+
+
+// Spawn the program in argv and call the callback when the program
+// return an exit code. The exit code is passed to the callback and if
+// its value is 0, the stdout of the program is also passed to the
+// callback. If the value of the exit code is different then 0, pass
+// stderr instead.
+function spawn_async_with_pipes(argv, callback) {
+    try {
+        var [success, pid, in_fd, out_fd, err_fd] =
+            GLib.spawn_async_with_pipes(null, argv, null,
+                                        GLib.SpawnFlags.SEARCH_PATH
+                                        | GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+                                        null);
+    } catch (err) {
+        Util._handleSpawnError(argv[0], err);
+    }
+
+
+    if (success) {
+        GLib.child_watch_add(GLib.PRIORITY_DEFAULT_IDLE, pid,
+            function (pid, status) {
+                var out_or_err = null;
+                var is_fatal_error = false;
+                var exitcode = 0;
+
+                try {
+                    GLib.spawn_check_exit_status(status);
+                } catch(err) {
+                    if (err instanceof GLib.Error &&
+                            err.domain === G_SPAWN_EXIT_ERROR) {
+                        exitcode = err.code;
+                    } else {
+                        Util._handleSpawnError(argv[0], err);
+                        is_fatal_error = true;
+                    }
+                }
+
+                if (!is_fatal_error) {
+                    let fd = (exitcode === 0) ? out_fd : err_fd;
+                    let reader = new Gio.DataInputStream({
+                        base_stream: new Gio.UnixInputStream({fd: fd})
+                    });
+
+                    [out_or_err] = reader.read_until("", null);
+                    callback(exitcode, out_or_err);
+                }
+
+                try {
+                    GLib.close(in_fd);
+                    GLib.close(out_fd);
+                    GLib.close(err_fd);
+                } catch(err) {
+                    // Do nothing if there is an error, because we need to
+                    // close the pid
+                }
+
+                GLib.spawn_close_pid(pid);
+            }
+        );
+    }
+}
 
 
 
 //----------------------------------------------------------------------
 // Logging
 //----------------------------------------------------------------------
-
+// UUID is defined in main
 function log(message) {
-    global.log(UUID + ": " + message)
+    global.log(UUID + ": " + message);
 }
 
 function logError(error) {
-    global.logError(UUID + ": " + error)
+    global.logError(UUID + ": " + error);
 }
 
 
@@ -77,8 +192,27 @@ MyApplet.prototype = {
                                    this.on_settings_changed,
                                    null);
 
-        this.applet_on_panel = true;
+        this.settings.bindProperty(Settings.BindingDirection.IN,
+                                  "do_autologin",
+                                  "do_autologin",
+                                  this.on_settings_changed,
+                                  null);
 
+        this.settings.bindProperty(Settings.BindingDirection.IN,
+                                "ok_color",
+                                "ok_color",
+                                this.on_settings_changed,
+                                null);
+
+        this.settings.bindProperty(Settings.BindingDirection.IN,
+                                "error_color",
+                                "error_color",
+                                this.on_settings_changed,
+                                null);
+
+        this.applet_on_panel = true;
+        this.clicked = false;
+        this.set_applet_label("Error");
         this.loop();
     },
 
@@ -86,48 +220,56 @@ MyApplet.prototype = {
 
     get_available: function() {
         // Let python handle the dirty work ;)
-        Util.spawn_async(
+        spawn_async_with_pipes(
             [
                 'python',
-                this.metadata["path"]+PYTHON_SCRIPT_NAME,
+                PYTHON_SCRIPT_PATH,
+                "get",
                 this.username, this.password
             ],
             Lang.bind(this, this.update_label)
         );
-
     },
 
 
 
-    update_label: function(response)
-    {
-        let new_label = "Error";
-
-        //remove newline
-        response = response.replace(/(\r\n|\n|\r)/gm, "")
-
-        if(response != "")
-        {
-             new_label = response;
+    update_label: function(exitcode, out_or_err) {
+        if (this.check_exitcode(exitcode, out_or_err)) {
+            let new_label = out_or_err.replace(/(\r\n|\n|\r)/gm, "");
+            this._applet_label.set_style("color:" + this.ok_color);
+            this.set_applet_label(new_label);
+        } else {
+            this._applet_label.set_style("color:" + this.error_color);
         }
-        else
-        {
-            logError("Nothing retrieved, please verify the user "    +
-                     "credentials provided in the configuration of " +
-                     "this applet");
-        }
-
-        this.set_applet_label(new_label);
-
     },
 
 
 
-    loop: function()
-    {
+    check_exitcode: function(exitcode, out_or_err) {
+        switch(exitcode) {
+            case 0:
+                return true;
+                break;
+
+            case 1:
+            case 2:
+                logError("error calling the python program: "
+                         + PYTHON_SCRIPT_NAME);
+                break;
+
+
+            default:
+                logError(out_or_err);
+        }
+
+        return false;
+    },
+
+
+
+    loop: function() {
         // The loop must stop if the applet is not on the pannel
-        if(this.applet_on_panel)
-        {
+        if (this.applet_on_panel) {
             this.get_available();
 
             Mainloop.timeout_add_seconds(
@@ -148,16 +290,37 @@ MyApplet.prototype = {
 
 
 
-    on_settings_changed: function()
-    {
+    on_settings_changed: function() {
         // Nothing here for now
 
     },
 
 
+    on_applet_clicked: function() {
+        // The clicked attribute is to make sure that multiple click on the
+        // applet wont break everything
+        if (!this.clicked) {
+            this.clicked = true;
+            let args = [
+                'python',
+                PYTHON_SCRIPT_PATH,
+                "open"
+            ]
 
-    on_applet_removed_from_panel: function()
-    {
+            if (this.do_autologin) {
+                args.push(this.username, this.password)
+            }
+
+
+            spawn_async(args, Lang.bind(this, function () {
+                this.clicked = false;
+            }));
+        }
+    },
+
+
+
+    on_applet_removed_from_panel: function() {
         // Timeouts created using Mainloop.timeout_add_seconds does not
         // stop automatically when the applet is removed from the panel
         // This flag is used to stop those timeouts
@@ -170,6 +333,9 @@ MyApplet.prototype = {
 // Main
 //----------------------------------------------------------------------
 function main(metadata, orientation, panel_height, instance_id) {
+    UUID = metadata["uuid"];
+    PYTHON_SCRIPT_PATH = metadata["path"] + "/" + PYTHON_SCRIPT_NAME;
+
     return new MyApplet(metadata,
                         orientation,
                         panel_height,
